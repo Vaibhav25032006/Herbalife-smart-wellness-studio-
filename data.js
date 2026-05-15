67af520017450d18338ba27bff4cf1502062a003
Vaibhav25032006 <vaibhavvashisth574@gmail.com>
2026-05-14 20:09:59 -0700

    Refactor sheetData initialization and cleanup

---
+++ data.js
@@ -0,0 +1,7 @@
+const sheetData=[
+{id:"1205175935",name:"Vaibhav Vashisth",age:20,weight:68,goal:"Muscular Body",bmi:23.5,category:"Normal Weight",ideal:65.9},
+{id:"1205172534",name:"Meenu Sharma",age:42,weight:67.6,goal:"Healthy Living",bmi:26.4,category:"Overweight",ideal:52.4},
+{id:"1205190731",name:"Hemant Sharma",age:51,weight:80,goal:"Weight Loss",bmi:27.7,category:"Overweight",ideal:65.9},
+{id:"1405100910",name:"User 1405",age:78,weight:85,goal:"Healthy Living",bmi:27.8,category:"Overweight",ideal:70.5},
+{id:"1205174000",name:"Alka Jha",age:34,weight:72,goal:"Weight Loss",bmi:28.1,category:"Overweight",ideal:58.0}
+];
